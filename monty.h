70ec404f434e455322c0d02143fab19d1e0be461
Jenny <5704@holbertonstudents.com>
2022-12-14 14:31:11 -0800

    Added in prototypes for new funcs

--- monty.h
+++ monty.h
@@ -38,6 +38,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-
+void pall(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void pop(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number);
 #endif
