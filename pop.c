c05daed05faa5ff627c21b9de3062e2435a4e4c5
Jenny <5704@holbertonstudents.com>
2022-12-14 14:51:32 -0800

    Edited || statement

--- pop.c
+++ pop.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
-void pop(stack_t **stack, __attribute__((unused))unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = NULL;
 
-	if (stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
