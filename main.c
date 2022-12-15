b66c02c58b8b0c965b94d2e686f122bf219773c5
Jenny <5704@holbertonstudents.com>
2022-12-14 20:41:41 -0800

    Added add func to Makefile, main and header

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
 		{"pint", pint},
 		{"pop", pop},
 		{"nop", nop},
+		{"add", add},
 		{NULL, NULL}
 	};
 
--- monty.h
+++ monty.h
@@ -42,5 +42,6 @@ void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, __attribute__((unused))unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 #endif
