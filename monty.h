271b77eee48ade5338ab72bd8560b88a25bd5323
Jenny <5704@holbertonstudents.com>
2022-12-14 22:08:08 -0800

    Updated header

--- monty.h
+++ monty.h
@@ -38,8 +38,8 @@ typedef struct instruction_s
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
