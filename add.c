b837dab75f86ecd365a182548064bdf49ac9a525
Jenny <5704@holbertonstudents.com>
2022-12-14 21:20:30 -0800

    Re-added statement for *stack == NULL

--- add.c
+++ add.c
@@ -2,7 +2,12 @@
 
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || (*stack)->next == NULL)
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
