81ddd1b99b099a32c166265b3e27cb426d84d6c2
rrarima <5708@holbertonstudents.com>
2022-12-14 17:50:10 -0800

    cleaned white space

--- func.c
+++ func.c
@@ -53,11 +53,10 @@ void pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || stack == NULL)
-	{	
+	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	printf("%i\n", (*stack)->n);
 }
-	
